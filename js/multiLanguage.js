3602489fec2543c6db3e2d656f286a07f35387a3
Jonas <jp.marcilio2@gmail.com>
2023-11-22 17:35:13 -0300

    0.1 multilanguage

--- js/multiLanguage.js
+++ js/multiLanguage.js
@@ -4,24 +4,68 @@ var dataReload = document.querySelectorAll("[data-reload]");
 //language translators
 var language = {
     eng: {
-        heroTitle: "We are a Programation NGO"
+        //S
+        SContactUs: "Contact Us",
+        //Navbar
+        navAbout: "Home",
+        navService: "Service",
+        navContact: "Contact",
+        navLanguage: "Language",
+        //Hero section
+        heroTitle: "We are a Programation NGO",
+        heroDescription: "We give the opportunity to people learn programation and IT."
+
     },
     pt: {
-        heroTitle: "Nós somos uma ONG de programação"
+        //S
+        SContactUs: "Nos contate",
+        //Navbar
+        navAbout: "Início",
+        navService: "Serviço",
+        navContact: "Contato",
+        navLanguage: "Linguagem",
+        //Hero section
+        heroTitle: "Nós somos uma ONG de programação",
+        heroDescription: "Nós damos oportunidade para pessoas aprenderem programação e TI."
     }
 }
 
 //define language
-if (window.location.hash) {
-    if (window.location.hash === "#pt") {
-        console.log("pt");
-        document.getElementById("hero-title").textContent = language.pt.heroTitle;
-    }
+setInterval(ChangeLanguage, 1);
+function ChangeLanguage () {
+    if (window.location.hash) {
+        if (window.location.hash == "#eng") {
+            //S
+            document.getElementById("s-contact-button").textContent = language.eng.SContactUs;
+            //Navbar
+            document.getElementById("nav-about").textContent = language.eng.navAbout;
+            document.getElementById("nav-service").textContent = language.eng.navService;
+            document.getElementById("nav-contact").textContent = language.eng.navContact;
+            document.getElementById("nav-language").textContent = language.eng.navLanguage;
+            //Hero section
+            document.getElementById("hero-title").textContent = language.eng.heroTitle;
+            document.getElementById("hero-description").textContent = language.eng.heroDescription;
+        }
+        else if (window.location.hash == "#pt") {
+            //S
+            document.getElementById("s-contact-button").textContent = language.pt.SContactUs;
+            //Navbar
+            document.getElementById("nav-about").textContent = language.pt.navAbout;
+            document.getElementById("nav-service").textContent = language.pt.navService;
+            document.getElementById("nav-contact").textContent = language.pt.navContact;
+            document.getElementById("nav-language").textContent = language.pt.navLanguage;
+            //Hero section
+            document.getElementById("hero-title").textContent = language.pt.heroTitle;
+            document.getElementById("hero-description").textContent = language.pt.heroDescription;
+        }
+    }    
 }
 
+/*
 //define language reload onclick illiterations
-for (i = 0; i <= dataReload.length; i++) {
+for (i = 0; i < dataReload.length; i++) {
     dataReload[i].onclick = function() {
-        location.reload (true);
+        location.reload(true);
     }
 }
+*/
